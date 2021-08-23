30388858875f698a26e238af38403db1c42b23d0
Kevin Su <kevinsu@outlook.co.nz>
2021-08-23 15:58:10 +1200

    country sort by population asc/dsc and name

--- src/Components/home.js
+++ src/Components/home.js
@@ -7,13 +7,33 @@ function Home() {
     const [countries, setCountries] = useState([]);
 
     useEffect(() => {
-        getCountries();
+        getCountries("alphabet");
     }, []);
 
-    const getCountries = async () => {
+    const getCountries = async term => {
         const res = await fetch('https://restcountries.eu/rest/v2/all');
         const data = await res.json();
         await setCountries(data);
+    }
+
+    const sortCountry = (term) => {
+        if(term === "descending"){
+            const countrySortByDscPop = [].concat(countries)
+                        .sort((a, b) => a.population > b.population ? 1 : -1);
+
+            setCountries(countrySortByDscPop);
+        }
+        if(term === "ascending"){
+            const countrySortByAscPop = [].concat(countries)
+                        .sort((a, b) => a.population < b.population ? 1 : -1);
+            setCountries(countrySortByAscPop);
+        }
+        if(term === "alphabet"){
+            const countrySortByName = [].concat(countries)
+                        .sort((a, b) => a.name > b.name ? 1 : -1);
+
+            setCountries(countrySortByName);
+        }
     }
 
     const filterByContinent = async continent => {
@@ -40,14 +60,20 @@ function Home() {
             </div>
              <div className="flex container mx-auto mb-16">
                 <i className="fa fa-search my-auto -mr-9 z-10 pr-2 pl-3 py-5 rounded-md text-gray-400"></i>
-                <input type="text" placeholder="Search for a country..." className="pl-10 p-2 shadow-md rounded-md w-1/3 dark:bg-gray-700" onChange={ term => searchCountry(term.target.value)} />
-                <select className="ml-auto my-2 p-2 shadow-md rounded-md font-medium dark:bg-gray-700" onChange={ val => filterByContinent(val.target.value)}>
-                    <option value="">Filter by Region</option>
+                <input type="text" placeholder="Search for a country..." className="pl-10 p-2 shadow-md rounded-md w-1/3" onChange={ term => searchCountry(term.target.value)} />
+                <select className="ml-auto my-2 p-2 shadow-md rounded-md font-medium" onChange={ val => filterByContinent(val.target.value)}>
+                    <option value="">Continent</option>
                     <option value="africa">Africa</option>
                     <option value="americas">America</option>
                     <option value="asia">Asia</option>
                     <option value="europe">Europe</option>
                     <option value="oceania">Oceania</option>
+                </select>
+                <select className="ml-10 my-2 p-2 shadow-md rounded-md font-medium" onChange={ val => sortCountry(val.target.value)}>
+                    <option value="">Sort</option>
+                    <option value="alphabet">Alphabetical</option>
+                    <option value="descending">Pop (dsc)</option>
+                    <option value="ascending">Pop (asc)</option>
                 </select>
             </div>
             <div className="container grid grid-cols-4 gap-16 mx-auto">
